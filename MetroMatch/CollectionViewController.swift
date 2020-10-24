b3461bbf0d23eee6ecddbcdb4b3fb2835b4048b8
Edwjon <edwardpizzurro@gmail.com>
2020-10-24 16:03:29 -0400

    Creación celdas Post

--- MetroMatch/CollectionViewController.swift
+++ MetroMatch/CollectionViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
+let posts: [Post] = [
+    Post(imagenPerfil: UIImage(named: "mia")!, nombre: "Mia Milano", imagenPrincipal: UIImage(named: "george")!, username: "@miamilano", descripcion: "Me parece muy inteligente y guapo!!!", comentarios: ["@andrea: Sii me parece lindísimo","@valeria: Sii guao me parece muy lindo", "@juancho: guao quien es esa jeva"]),
+    Post(imagenPerfil: UIImage(named: "stevenn")!, nombre: "Steven", imagenPrincipal: UIImage(named: "johana")!, username: "@steven", descripcion: "Johana me parece super mega linda, es mega increible!!!", comentarios: ["@jonathan: si es linda","@juancho: guao quien es esa jeva", "@juancho: guao quien es esa jeva"])
+]
 
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let reuseIdentifier = "Cell"
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Hola
-        collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView!.register(PostCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 
 
@@ -31,24 +38,52 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 7
+        return posts.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PostCell
     
-        cell.backgroundColor = UIColor.orange
+        cell.imagenPerfil.image = posts[indexPath.item].imagenPerfil
+        cell.nombreUsuario.text = posts[indexPath.item].nombre
+        cell.imagenGrande.image = posts[indexPath.item].imagenPrincipal
+        cell.usuarioLabel.text = posts[indexPath.item].username
+        cell.descripcion.text = posts[indexPath.item].descripcion
+        
+        //cell.tableView.numberOfRows(inSection: posts[indexPath.item].comentarios.count)
+//        cell.tableView.cellForRow(at: indexPath)?.textLabel = posts[indexPath.item].comentarios[indexPath.item]
+        
+
     
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let size = CGSize(width: collectionView.frame.width, height: 300)
+//        let aproximateWidthOfBioTextView = view.frame.width - 40
+//        let size = CGSize(width: aproximateWidthOfBioTextView, height: 1000)
+//        let attributes = [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15)]
+//
+//        let estimatedFrame = NSString(string: posts[indexPath.item].descripcion).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+//
+//        return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        
+        let size = CGSize(width: collectionView.frame.width, height: 500)
         return size
     }
     
     
 
+}
+
+
+
+struct Post {
+    let imagenPerfil: UIImage
+    let nombre: String
+    let imagenPrincipal: UIImage
+    let username: String
+    let descripcion: String
+    let comentarios: [String]
 }
---
+++ MetroMatch/PostCell.swift
@@ -0,0 +1,178 @@
+//
+//  PostCell.swift
+//  MetroMatch
+//
+//  Created by Edward Pizzurro Fortun on 10/22/20.
+//  Copyright © 2020 Edwjon. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UICollectionViewCell, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let viewArriba: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.shadowColor = UIColor.gray.cgColor
+        view.layer.shadowOpacity = 0.9
+        view.layer.shadowOffset = CGSize.zero
+        view.layer.shadowRadius = 6
+        return view
+    }()
+    
+    
+    let imagenPerfil: UIImageView = {
+        let imagen = UIImageView()
+        //imagen.backgroundColor = .red
+        imagen.layer.cornerRadius = 25
+        imagen.clipsToBounds = true
+        imagen.contentMode = .scaleToFill
+        imagen.translatesAutoresizingMaskIntoConstraints = false
+        imagen.layer.shadowColor = UIColor.gray.cgColor
+        imagen.layer.shadowOpacity = 0.9
+        imagen.layer.shadowOffset = CGSize.zero
+        imagen.layer.shadowRadius = 6
+        return imagen
+    }()
+    
+    let nombreUsuario: UILabel = {
+        let label = UILabel()
+        label.text = "Usuario"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        //label.backgroundColor = .purple
+        return label
+    }()
+    
+    let viewCuadrada: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = UIColor.lightGray.cgColor
+//        view.layer.shadowColor = UIColor.gray.cgColor
+//        view.layer.shadowOpacity = 0.9
+//        view.layer.shadowOffset = CGSize.zero
+//        view.layer.shadowRadius = 6
+        return view
+    }()
+    
+    let separador: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    let imagenGrande: UIImageView = {
+        let imagen = UIImageView()
+        //imagen.backgroundColor = .green
+        imagen.layer.cornerRadius = 10
+        imagen.contentMode = .scaleToFill
+        imagen.clipsToBounds = true
+        imagen.translatesAutoresizingMaskIntoConstraints = false
+        return imagen
+    }()
+    
+    let usuarioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = UIColor(red: 248/255, green: 150/255, blue: 166/255, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
+    let descripcion: UITextView = {
+        let tv = UITextView()
+        tv.isEditable = false
+        tv.backgroundColor = .clear
+        //tv.layer.cornerRadius = 10
+        tv.font = UIFont.systemFont(ofSize: 16)
+        tv.isUserInteractionEnabled = false
+        //tv.text = "Hola esta es mi descripcion del mensaje jajajajajajjajajajajajjajajajajajjajajajajajjajajajajajjajajajajajjajajajajajjajajajajajjajajajajajjajajajajaj"
+        //tv.backgroundColor = .yellow
+        return tv
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.allowsSelection = false
+        tv.delegate = self
+        tv.dataSource = self
+        tv.layer.cornerRadius = 10
+        tv.separatorStyle = .none
+        tv.backgroundColor = UIColor(red: 248/255, green: 150/255, blue: 166/255, alpha: 1)
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+//        addSubview(imagenPerfil)
+//        imagenPerfil.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 30, leftConstant: 30, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+//
+//        addSubview(nombreUsuario)
+//        nombreUsuario.anchor(nil, left: imagenPerfil.rightAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 10, bottomConstant: 0, rightConstant: 20, widthConstant: frame.width - 30 - 50 - 30 - 10, heightConstant: 35)
+//        nombreUsuario.centerYAnchor.constraint(equalTo: imagenPerfil.centerYAnchor, constant: 0).isActive = true
+//
+//        addSubview(viewCuadrada)
+//        viewCuadrada.anchor(imagenPerfil.bottomAnchor, left: imagenPerfil.leftAnchor, bottom: nil, right: nombreUsuario.rightAnchor, topConstant: 10, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 400)
+        
+        addSubview(viewArriba)
+        viewArriba.anchor(topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, topConstant: 20, leftConstant: 20, bottomConstant: 0, rightConstant: 20, widthConstant: 0, heightConstant: 50)
+        
+        viewArriba.addSubview(imagenPerfil)
+        imagenPerfil.anchor(viewArriba.topAnchor, left: viewArriba.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        viewArriba.addSubview(nombreUsuario)
+        nombreUsuario.anchor(nil, left: imagenPerfil.rightAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 10, bottomConstant: 0, rightConstant: 0, widthConstant: frame.width - 30 - 50 - 30 - 10, heightConstant: 35)
+        nombreUsuario.centerYAnchor.constraint(equalTo: imagenPerfil.centerYAnchor, constant: 0).isActive = true
+        
+        addSubview(viewCuadrada)
+        viewCuadrada.anchor(imagenPerfil.bottomAnchor, left: imagenPerfil.leftAnchor, bottom: nil, right: nombreUsuario.rightAnchor, topConstant: 15, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 400)
+        viewCuadrada.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        
+        viewCuadrada.addSubview(imagenGrande)
+        imagenGrande.anchor(viewCuadrada.topAnchor, left: viewCuadrada.leftAnchor, bottom: nil, right: viewCuadrada.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 200)
+        
+        viewCuadrada.addSubview(usuarioLabel)
+        usuarioLabel.anchor(imagenGrande.bottomAnchor, left: viewCuadrada.leftAnchor, bottom: nil, right: imagenGrande.rightAnchor, topConstant: 12, leftConstant: 8, bottomConstant: 0, rightConstant: 8, widthConstant: 0, heightConstant: 20)
+        
+        viewCuadrada.addSubview(descripcion)
+        descripcion.anchor(usuarioLabel.bottomAnchor, left: usuarioLabel.leftAnchor, bottom: nil, right: usuarioLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 50)
+        
+        viewCuadrada.addSubview(tableView)
+        tableView.anchor(descripcion.bottomAnchor, left: descripcion.leftAnchor, bottom: viewCuadrada.bottomAnchor, right: descripcion.rightAnchor, topConstant: 10, leftConstant: 0, bottomConstant: 8, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+//        addSubview(separador)
+//        separador.anchor(nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 10, leftConstant: 10, bottomConstant: 0, rightConstant: 10, widthConstant: 0, heightConstant: 0.5)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor(red: 248/255, green: 150/255, blue: 166/255, alpha: 1)
+        
+        cell.textLabel?.text = posts[indexPath.item].comentarios[indexPath.item]
+        return cell
+    }
+    
+}
+
+
+
