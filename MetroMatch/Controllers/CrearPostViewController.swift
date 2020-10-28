94bbd40636e3c703454d1e9af979500f01dbc2df
Reichel <reichel.larez@correo.unimet.edu.ve>
2020-10-28 01:20:16 -0400

    create post logic

--- MetroMatch/Controllers/CrearPostViewController.swift
+++ MetroMatch/Controllers/CrearPostViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
 
 class CrearPostViewController: UIViewController {
 
@@ -23,6 +25,12 @@ class CrearPostViewController: UIViewController {
         title = "Crear"
         
         setupInterface()
+    }
+    let db = Firestore.firestore()
+    
+    func randomCompatibility() -> Int{
+        let number = 0 + arc4random_uniform(100 - 0 + 1)
+        return Int(number)
     }
     
     func setupInterface() {
@@ -54,6 +62,56 @@ class CrearPostViewController: UIViewController {
     }
     
     @IBAction func publicar(_ sender: Any) {
+        var crushID = ""
+        guard let username = usernameTextField.text, !username.isEmpty,
+              let description = descripcionTextView.text, !description.isEmpty
+        else{
+            print("Debe llenar todos los campos")
+            return
+        }
+        print(description)
+        db.collection("users").whereField("username", isEqualTo: username).getDocuments(){(result, err) in
+            if let err = err {
+                print("No existe el nombre de usuario ingresado, por favor ingrese un nombre de usuario válido", err)
+            } else {
+                print("Se obtuvo un resultado")
+                for document in result!.documents {
+                    crushID = document.documentID
+                }
+                let compatibility = self.randomCompatibility()
+                print(compatibility)
+                let user = Auth.auth().currentUser
+                if user != nil {
+                    let postData: [String: Any] = [
+                        "comments": [],
+                        "compatibility": compatibility,
+                        "creatorID": user!.uid,
+                        "crushID": crushID,
+                        "description": description,
+                        "id": "",
+                    ]
+                    var ref: DocumentReference? = nil
+                    ref = self.db.collection("posts").addDocument(data: postData){ err in
+                        if let err = err {
+                            print("No se creo el post", err)
+                        } else {
+                            self.db.collection("posts").document(ref!.documentID).updateData([
+                                "id": ref!.documentID,
+                            ]){err in
+                                if let err = err {
+                                    print("El post no se creo correctamente", err)
+                                } else {
+                                    print("El post se creo correctamente")
+                                }
+                            }
+                            
+                        }
+                    }
+                } else {
+                  print("No se ha iniciado sesión")
+                }
+            }
+        }
         self.crearTabBar()
         //performSegue(withIdentifier: "publicarSegue", sender: self)
     }
