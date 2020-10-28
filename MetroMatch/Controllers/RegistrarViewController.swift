187724fc1bbdcaee44030ce93adabf452588110b
Edwjon <edwardpizzurro@gmail.com>
2020-10-28 09:25:18 -0400

    Añadir inputs al registro

--- MetroMatch/Controllers/RegistrarViewController.swift
+++ MetroMatch/Controllers/RegistrarViewController.swift
@@ -15,9 +15,14 @@ class RegistrarViewController: UIViewController {
 
     @IBOutlet var registroLabel: UILabel!
     
-    @IBOutlet var usernameTextField: UITextField!
     
+    @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var nombreTextField: UITextField!
+    @IBOutlet var apellidoTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    
+    
     
     @IBOutlet var registrarButton: UIButton!
     
@@ -29,6 +34,7 @@ class RegistrarViewController: UIViewController {
     }
     
     let db = Firestore.firestore()
+    
     func setupInterface() {
         //UserName TExtField
         usernameTextField.placeholder = "Ingresa tu username"
@@ -53,21 +59,45 @@ class RegistrarViewController: UIViewController {
         let colorIzquierdaAmarillo = UIColor(red: 1, green: 0.73, blue: 0.004, alpha: 1)
         let colorDerechaNaranja = UIColor(red: 0.998, green: 0.417, blue: 0.298, alpha: 1)
         registrarButton.setGradientBackground(colorOne: colorDerechaNaranja, colorTwo: colorIzquierdaAmarillo)
+        
+        nombreTextField.font = UIFont.systemFont(ofSize: 16) //Aqui va el font del usernameTextfield
+        nombreTextField.layer.masksToBounds = true
+        nombreTextField.layer.cornerRadius = 12
+        
+        apellidoTextField.font = UIFont.systemFont(ofSize: 16) //Aqui va el font del usernameTextfield
+        apellidoTextField.layer.masksToBounds = true
+        apellidoTextField.layer.cornerRadius = 12
+        
+        emailTextField.font = UIFont.systemFont(ofSize: 16) //Aqui va el font del usernameTextfield
+        emailTextField.layer.masksToBounds = true
+        emailTextField.layer.cornerRadius = 12
     }
     
 
     func constrains() {
         
         registroLabel.translatesAutoresizingMaskIntoConstraints = false
-        registroLabel.anchor(view.topAnchor, left: nil, bottom: nil, right: nil, topConstant: 200, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 2, heightConstant: 40)
+        registroLabel.anchor(view.topAnchor, left: nil, bottom: nil, right: nil, topConstant: 150, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 2, heightConstant: 40)
         registroLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
+        nombreTextField.translatesAutoresizingMaskIntoConstraints = false
+        nombreTextField.anchor(registroLabel.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 40, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
+        nombreTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        apellidoTextField.translatesAutoresizingMaskIntoConstraints = false
+        apellidoTextField.anchor(nombreTextField.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 30, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
+        apellidoTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         usernameTextField.translatesAutoresizingMaskIntoConstraints = false
-        usernameTextField.anchor(registroLabel.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 40, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
+        usernameTextField.anchor(apellidoTextField.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 30, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
         usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.anchor(usernameTextField.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 40, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
+        emailTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        passwordTextField.anchor(usernameTextField.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 30, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
+        passwordTextField.anchor(emailTextField.bottomAnchor, left: nil, bottom: nil, right: nil, topConstant: 30, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: view.frame.width / 1.3, heightConstant: 40)
         passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         registrarButton.translatesAutoresizingMaskIntoConstraints = false
@@ -80,21 +110,24 @@ class RegistrarViewController: UIViewController {
     
     
     @IBAction func registrar(_ sender: Any) {
-        let layout = UICollectionViewFlowLayout()
-        guard let username = usernameTextField.text, !username.isEmpty,
-              let password = passwordTextField.text, !password.isEmpty else {
+        //let layout = UICollectionViewFlowLayout()
+        guard let email = emailTextField.text, !email.isEmpty,
+            let password = passwordTextField.text, !password.isEmpty,
+            let username = usernameTextField.text, !username.isEmpty,
+            let nombre = nombreTextField.text, !nombre.isEmpty,
+            let apellido = apellidoTextField.text, !apellido.isEmpty else{
             print("Faltan campos para completar")
             return
         }
         
-        Auth.auth().createUser(withEmail: username, password: password) { authResult, error in
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             guard error == nil else {
                 // show account creation
                 
                 return
             }
             self.db.collection("users").document((authResult?.user.uid)!).setData([
-                "email": username
+                "email": email
             ]){ err in
                 if let err = err {
                     print("No se creo el usuario: ", err)
